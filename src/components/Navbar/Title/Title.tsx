9ac1c53c8f29cc06400e1bc7906d933c31080a6b
dombonifacio <nikkibonifacio184@gmail.com>
2024-05-07 19:51:21 -0700

    Changed the tittle components to borders and made it responsive.

--- src/components/Navbar/Title/Title.tsx
+++ src/components/Navbar/Title/Title.tsx
@@ -1,12 +1,14 @@
 const Title = () => {
   return (
     <>
-      
-        <h1 className="text-[2.5rem] lg:text-[2.8rem] font-extrabold relative inline-block z-50  ">
-          Skills
-        </h1>
-        <div className="absolute bg-rose p-2 w-[7.5rem] bottom-8 left-10 md:left-12  md:bottom-[2.4rem] lg:w-[8rem] lg:bottom-10 h-4  lg:left-14  xl:bottom-10 xl:-left-3 xl:w-32"></div>
-  
+     
+        <div className=" border-rose inline-block border-b-[16px] px-[24px] leading-[1rem] w-[6.8rem] md:w-32">
+          
+          <h1 className="text-[2.4rem] md:text-[2.6rem] lg:text-[2.8rem] font-extrabold  ml-2 md:ml-6  ">
+            Skills
+          </h1>
+        </div>
+   
     </>
   );
 };
